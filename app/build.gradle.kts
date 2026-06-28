bbfe5e23ff48b68a2fc0bab78e62e91b703a13f4
Pyae Sone Shin Thant <pssthant@gmail.com>
2026-06-28 15:06:25 +0700

    chore: bump version to 0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.psst.orbit"
         minSdk = 21
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.7.0"
     }
 
     signingConfigs {
